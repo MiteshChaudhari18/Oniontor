28ab9cc9f4cc21e0e90fc7903403c1281bbb2cc0
Mitesh Chaudhari <Krunal2012003@gmail.com>
2025-11-03 21:32:07 +0530

    Use background.mp4 video for all pages instead of only home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 // src/App.tsx
-import { Switch, Route, useLocation } from "wouter";
+import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -20,39 +20,22 @@ function Router() {
 }
 
 function App() {
-  const [location] = useLocation();
-  const isHome = location === "/";
-
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
         <TooltipProvider>
           <div className="relative min-h-screen text-white">
-            {/* Background: video for home, image for others */}
-            {isHome ? (
-              <>
-                <video
-                  autoPlay
-                  loop
-                  muted
-                  playsInline
-                  className="fixed inset-0 w-full h-full object-cover -z-10"
-                >
-                  <source src="/background.mp4" type="video/mp4" />
-                </video>
-                <div className="fixed inset-0 bg-black/40 -z-10" />
-              </>
-            ) : (
-              <>
-                <div
-                  className="fixed inset-0 bg-cover bg-center -z-10"
-                  style={{
-                    backgroundImage: `url('/background.png')`,
-                  }}
-                />
-                <div className="fixed inset-0 bg-black/40 -z-10" />
-              </>
-            )}
+            {/* Background: video for all pages */}
+            <video
+              autoPlay
+              loop
+              muted
+              playsInline
+              className="fixed inset-0 w-full h-full object-cover -z-10"
+            >
+              <source src="/background.mp4" type="video/mp4" />
+            </video>
+            <div className="fixed inset-0 bg-black/40 -z-10" />
 
             {/* Foreground */}
             <div className="relative z-10">
